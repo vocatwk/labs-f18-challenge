16ee71d08ac316d7384e358fa401ba8f45c5bd04
Veronica Woldehanna <vocatwk0@gmail.com>
2018-09-07 11:06:33 -0400

    Added working dynamic endpoint

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,20 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<query>/')
+def index(query):
+	request = requests.get('http://pokeapi.co/api/v2/pokemon/' + query)
+
+	# check if pokemon info for query exists
+	if request.status_code != 200:
+		return render_template('errorPage.html')
+
+	if query.isnumeric():
+		name = request.json()['name']
+		return render_template('pokemonNameInfo.html', name=name, id=query)
+	else:
+		id = str(request.json()['id'])
+		return render_template('pokemonIdInfo.html', name=query, id=id)
 
 if __name__ == '__main__':
     app.run()
